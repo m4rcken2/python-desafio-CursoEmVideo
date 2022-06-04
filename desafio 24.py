e220fec4e8553231af9d7e10e07de5e7740607db
m4rcken2 <marckendymondelus12@gmail.com>
2022-06-04 12:05:54 -0300

    desafio 24

---
+++ desafio 24.py
@@ -0,0 +1,5 @@
+###Exercício Python 24: ###
+# Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”. 
+
+cidade = str(input('Em qual cidade você mora? ')).strip()
+print(cidade[:5].lower() == 'santo')
